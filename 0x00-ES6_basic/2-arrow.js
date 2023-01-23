b85e682de18751be38f29b665a3b3969d37d4548
Mikelesnr <mwanza.n.m@gmail.com>
2023-01-23 08:27:13 +0200

    changing regular function to arrow function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+#!/usr/Bin/node
+//jshint esversion:6
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
