15637751f145fcad21cd6c21ed3c4cc8b5d69b96
Mikelesnr <mwanza.n.m@gmail.com>
2023-01-23 08:20:40 +0200

    replacing var with const to prevent overwritting of variables

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+#!/usr/Bin/node
+//jshint esversion:6
+
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
+  }
+
+  return [task, task2];
+}
